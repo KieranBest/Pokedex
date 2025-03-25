4eaead1eac2c63a852a36ee2215ed7d98e14a944
Kieran Best <best.kieran.4595@gmail.com>
2025-03-25 21:37:28 +0000

    Dynamic background colours

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -40,9 +40,29 @@ export const Home = () => {
   function handleSearchInput(input) {
     let filteredPokemon = pokemonData.filter(pokemon => pokemon.name.toLowerCase().includes(input.toLowerCase()));
     setFilteredPokemonData(filteredPokemon);
-    console.log(pokemonData)
+    console.log(filteredPokemon)
   }
 
+  const colourTypes = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+  };
   return (
     <div>
       <p className=' text-6xl'>Pokemon</p>
@@ -56,10 +76,13 @@ export const Home = () => {
       {loading ? <p>Loading...</p> : (
         <div className='grid grid-cols-10 pb-10 gap-4'>
           {filteredPokemonData.map((p) => (
-            <div className='bg-blue-100 hover:cursor-pointer' key={p.id} onClick={() => openSinglePokemon(p.id)}>
-              <img className='justcenter' src={p.sprites.front_default} alt={p.name} />
+            <div className='bg-blue-100 hover:cursor-pointer'
+              style={{backgroundColor: `${colourTypes[p.types[0].type.name]}`}}
+              key={p.id} onClick={() => openSinglePokemon(p.id)}
+            >
+              <img className='bg-white m-2 rounded-full' src={p.sprites.front_default} alt={p.name} />
               <p>{p.name}</p>
-              <p># {p.id}</p>
+              <p># {p.id} {p.types[0].type.name}</p>
             </div>
           ))}
         </div>
